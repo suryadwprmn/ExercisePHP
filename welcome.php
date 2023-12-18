d3a4e419684360ba721493f21f8e863fae72bf65
suryadwprmn <suryadwipramana18@gmail.com>
2023-12-19 06:52:49 +0700

    tugas php

---
+++ welcome.php
@@ -0,0 +1,16 @@
+<html>
+    <style>
+        span{
+            color: #e0caca;
+        }
+    </style>
+<body>
+
+<span>Selamat Datang </span><?php echo $_POST["name"]; ?><br>
+Email <?php echo $_POST ["email"]?><br>
+Website Anda <?php $_POST["website"]?><br>
+Cooment anda <?php $_POST["comment"]?><br>
+gender anda <?php $_POST["gender"]?>
+
+</body>
+</html>
